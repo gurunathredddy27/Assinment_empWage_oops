997daeb34dcbd58d9d8b479cd58cc8eac4cd293a
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2023-03-31 10:26:12 +0530

    presentAbsent

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+        Random random = new Random();
+        boolean isPresent = random.nextBoolean();
+
+        if(isPresent){
+            System.out.println(" Employee is Present");
+        }else {
+            System.out.println("Employee is absent");
+        }
     }
 }
